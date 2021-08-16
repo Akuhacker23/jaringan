3deb64cec7e369a4123aeaa796a96b43a416a268
Akuhacker23 <88781336+Akuhacker23@users.noreply.github.com>
2021-08-16 23:01:57 +0700

    Create jaringan.sh

---
+++ Punyaraihan/jaringan.sh
@@ -0,0 +1,50 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay by  Rehan
+echo $green"Selamat Datang"
+echo
+sleep 2
+echo $yellow"silahkan pilih toolsnya :"
+echo
+echo $cyan"1.) Stabilkan  Jaringan"
+echo $blue"0.) Keluar wae !!"
+echo
+echo $purple"2.) Matikan Lampu Rumah Tetangga Sebelah"
+echo $white
+read -p "PilihMana  : " bro
+if [ $bro =  1  ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Cyber
+echo $green"Stabilkan Jaringan by Cyber"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = ] ||  [  $bro  = 0 ]
+then
+clear
+echo $yellow
+figlet "bye"
+echo $white"To Be Continue"
+sleep 3
+exit
+fi
+
+if [ $bro = ] ||  [  $bro  = 2  ]
+then
+clear
+echo $yellow
+figlet "wkwk"
+echo $white"Tapi Boong"
+sleep 4
+exit
+fi
